c19688f2984a69da7f8038de18d7c1822ee83979
SAMER FARES <123877088+FARESsamer23@users.noreply.github.com>
2024-07-23 13:07:35 -1200

    install react js and taillwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-slate-500 w-max font-semibold rounded-md p-5 m-3 text-rose-400 text-lg '>
+      Fares samer</h1>
+    </>
+  )
+}
+
+export default App
