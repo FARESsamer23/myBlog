c68884f8de8725585e567ba8439d61c03dd6aa98
SAMER FARES <123877088+FARESsamer23@users.noreply.github.com>
2024-07-23 14:55:47 -1200

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,30 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl bg-slate-500 w-max font-semibold rounded-md p-5 m-3 text-rose-400 text-lg '>
+    <BrowserRouter>
+      <h1 className='text-3xl bg-red-500 w-max font-semibold rounded-md p-5 m-3 text-yellow-100   '>
       Fares samer</h1>
-    </>
+
+      <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/about" element={<About/>}/>
+       <Route path="/sign-in" element={<SignIn/>}/>
+       <Route path="/sign-up" element={<SignUp/>}/>
+       <Route path="/projects" element={<Projects/>}/>
+       <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
