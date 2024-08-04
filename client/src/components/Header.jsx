745df499966fa5e6e2bc6af3a8ff4593c12e3ae2
SAMER FARES <123877088+FARESsamer23@users.noreply.github.com>
2024-08-03 19:24:02 -1200

    add functionality to the sign up page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-lg
          font-semibold dark:text-white"
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-red-400  h-12 rounded-md
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-blue-800  h-12 rounded-md
          text-white">
           Fares&apos;s Blog
         </span>
