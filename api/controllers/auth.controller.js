3bea1507d4c7c7df1d6e86348f3fc60b4bddedb9
SAMER FARES <123877088+FARESsamer23@users.noreply.github.com>
2024-08-07 16:13:10 -1200

    create sign in ai route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User.model.js'
 import bcryptjs from "bcryptjs"
 import { errorHandler } from '../utils/error.js';
+ import jwt from 'jsonwebtoken'
 export const signup = async (req,res,next)=>{
    const {username , email , password} = req.body;
     if(!username || !email || !password || username ==='' || email ==='' || password ===''){
@@ -22,4 +23,29 @@ export const signup = async (req,res,next)=>{
       }
     }
 
+
+    export const signin = async (req,res,next)=>{
+      // check the output from user
+      const {email  , password} = req.body;
+    if(!email || !password || email ==='' || password ===''){
+         next(errorHandler(400,'All fields are required'))
+    } 
+        try{
+          const valideUser = await User.findOne({email})
+            if(!valideUser){
+             return next(errorHandler(404,'User not found'))
+            }
+            const validPassword = bcryptjs.compareSync(password,valideUser.password)
+            if(!validPassword){
+             return next(errorHandler(401,'Invalid password'))
+            }
+            const token = jwt.sign( {id:valideUser._id}, process.env.SECRET_KEY,);
+           const {password: pass, ...rest}= valideUser._doc
+          
+            res.status(200).cookie('access_token',token,{httpOnly:true})
+            .json(rest)
+          }catch(error){
+          next(error)
+        }
+    }
 
