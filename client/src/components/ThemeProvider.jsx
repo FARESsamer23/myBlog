259cbf0b7083862660ac4998a2824fa494ff302f
SAMER FARES <123877088+FARESsamer23@users.noreply.github.com>
2024-08-26 19:05:54 -1200

    add dark mode functionality

---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useSelector} from 'react-redux'
+
+// eslint-disable-next-line react/prop-types
+export default function ThemeProvider({ children }) {
+    const {theme} = useSelector(state => state.theme)
+  return (
+    <div className={theme}>
+    <div className=' bg-white text-gray-700 dark:text-gray-200 dark:bg-slate-900 min-h-screen'>
+    {children}
+    </div>
+   
+    </div>
+  )
+}
