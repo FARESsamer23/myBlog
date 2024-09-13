020f26eba526cb9fb1e876d6dbfb4264e91a4b59
SAMER FARES <123877088+FARESsamer23@users.noreply.github.com>
2024-09-12 22:05:29 -1200

    complete  user image upload funcyionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,21 +1,121 @@
-import { Avatar, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import React, { useState } from 'react'
+import { Alert, Avatar, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import {app} from '../firebase'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 export default function DashProfile() {
+    const [imageFile, setImageFile] = useState(null);
     const [formData, setFormData] = useState({});
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const filePeckerRef = useRef()
+    const [imageFileUplaodinProgress,setImageFileUplaodinProgress] = useState(0)
+    
+    const [imageFileUplaodinError,setImageFileUplaodinError] = useState(null)
+    console.log(imageFileUplaodinProgress,imageFileUplaodinError)
+    
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
       };
+
+   const handleImageChange = (e)=>{
+    const file = e.target.files[0]
+    if(file){
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+   }
+
+
+   
+   useEffect(()=>{
+
+       if(imageFile){
+          uploadImage();
+        }
+   },[imageFile]);
+
+   const uploadImage = async () =>{
+
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write : if
+    //       request.resource.size < 2 * 1024 * 1024 &&
+    //       request.resource.contentType.matches('image/.*')
+          
+    //     }
+    //   }
+
+     const storage  = getStorage(app);
+     const fileName = new Date().getTime() + imageFile.name;
+     const storageRef = ref(storage,fileName);
+     const uploadTask = uploadBytesResumable(storageRef,imageFile);
+     uploadTask.on('state_changed',
+        (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setImageFileUplaodinProgress(progress.toFixed(0))
+     },(error)=>{
+      setImageFileUplaodinError('Could not upload image (file must be less than 2MB )')
+     },
+     ()=>{
+          getDownloadURL(uploadTask.snapshot.ref).then(
+            (downloadURL) => {
+              setImageFileUrl(downloadURL)
+            }
+          )
+     }
+    );
+  }
+
+
     const {currentUser} = useSelector(state => state.user)
+
   return (
+
     <div className='max-w-lg mx-auto p-3 w-full'>
        <h1 className=' my-7 text-3xl font-semibold w-full text-center'>Profile</h1>
         
        <form className='flex flex-col gap-4 '>
-            <div className='w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden'>         
-             <img src={currentUser.profilePicture}  alt='user' 
-             className='rounded-full w-full h-full object-cover
-             border-8 border-[lightgray]'/></div>
+
+              <input hidden type='file' accept='image/*' onChange={handleImageChange} ref={filePeckerRef}/>
+           
+              <div className=' relative w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden' 
+                onClick={()=>{filePeckerRef.current.click()}}>  
+
+                 
+                <img src={imageFileUrl || currentUser.profilePicture}  alt='user' 
+                 className='rounded-full w-full h-full object-cover
+                  border-8 border-[lightgray]'/>
+                  { imageFileUplaodinProgress && (
+                    <CircularProgressbar value={imageFileUplaodinProgress ||0} 
+                    text={`${imageFileUplaodinProgress}%`} 
+                    strokeWidth={5}
+                    styles={{
+                      root: { 
+                         height:'100%',
+                        width:'100%',
+                        position:'absolute',
+                        top:0,
+                         left:0,      
+                        },
+                        path:{
+                          stroke:`rgba(62,152,199,${imageFileUplaodinProgress / 100})`
+                        }
+                    }}
+                    />)
+                  }
+              </div>
+              {
+                imageFileUplaodinError &&
+                <Alert color="Failure">
+                {imageFileUplaodinError}
+                </Alert>
+              }
+              
          
              <div>
              <Label value="Your name" />
